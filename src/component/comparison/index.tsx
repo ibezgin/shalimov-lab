c52d159b6fbf53f67a060cbf3d8c260d953bd10e
shadmad <shadmad@ya.ru>
2020-11-18 22:30:05 +0300

    косметика

--- src/component/comparison/index.tsx
+++ src/component/comparison/index.tsx
@@ -127,7 +127,7 @@ export const Comparison = React.memo(() => {
         }}
       </Formik>
       <Card title="Совпадение строк в процентном соотношении:" bordered={false} style={{ width: 500 }}>
-        <Progress percent={percent} type="circle" width={300} format={percent => percent===100 ? "Совпадают" : `${percent}%`} />
+        <Progress percent={percent} type="circle" width={300} format={percent => `${percent}%`} />
       </Card>
       {/* <Title level={4} >Совпадение строк в процентном соотношении:</Title> */}
       {/* <Progress percent={percent} type="circle" width={400} format={percent => percent===100 ? "Совпадают" : `${percent}%`} /> */}
